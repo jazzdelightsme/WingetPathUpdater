5e5f9e90e8253c738cf0abae3fd439e105fa6575
Dan Thompson (SBS) <danthom@microsoft.com>
2023-08-07 18:16:20 -0700

    Oh! We need an ARP entry for uninstall.

--- WingetPathUpdaterInstall.ps1
+++ WingetPathUpdaterInstall.ps1
@@ -14,12 +14,13 @@ param( [switch] $Force,
        [Parameter( Mandatory, ParameterSetName = 'Interactive' )]
        [switch] $Interactive,
 
-       [switch] $InstallTests
+       [switch] $InstallTests,
+
+       [switch] $Uninstall
      )
 
 try
 {
-
     Write-Verbose "Force: $Force"
     Write-Verbose "Silent: $Silent"
     Write-Verbose "SilentWithProgress: $SilentWithProgress"
@@ -27,17 +28,39 @@ try
 
     [bool] $quiet = $Silent -or $SilentWithProgress
 
+    function Test-Administrator
+    {
+        [CmdletBinding()]
+        param()
+
+        try
+        {
+            $currentUser = [Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())
+            return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+        }
+        finally { }
+    }
+
+    # If we are run from winget/ARP, we should be elevated; but in case anybody tries to
+    # run this manually...
+    if( !(Test-Administrator) )
+    {
+        throw "This script must be run elevated."
+    }
+
     $baseUrl = 'https://raw.githubusercontent.com/jazzdelightsme/WingetPathUpdater/main/'
 
     $fileUrls = [ordered] @{
         # We do the wingetHelper.ps1 first, because if there is a problem getting that,
         # the other two won't work.
         "$env:SystemRoot\System32\wingetHelper.ps1" = "${baseUrl}WingetHelper.ps1"
+        # The install script is pretty important, too (for uninstall):
+        "$env:SystemRoot\System32\WingetPathUpdaterInstall.ps1" = "${baseUrl}WingetPathUpdaterInstall.ps1"
         "$env:SystemRoot\System32\winget.ps1" = "${baseUrl}Winget.ps1"
         "$env:SystemRoot\System32\winget.cmd" = "${baseUrl}Winget.cmd"
     }
 
-    if( $InstallTests )
+    if( $InstallTests -or $Uninstall )
     {
         $fileUrls[ "$env:SystemRoot\System32\wingetHelper.Tests.ps1" ] = "${baseUrl}WingetHelper.Tests.ps1"
     }
@@ -48,7 +71,7 @@ try
     {
         if( (Test-Path $path) )
         {
-            if( !$quiet )
+            if( !$quiet -and !$Uninstall )
             {
                 Write-Host "Found existing file: " -Fore DarkGray -NoNewline
                 Write-Host $path -Fore DarkYellow -NoNewline
@@ -59,7 +82,7 @@ try
             # -Force.
             if( (Get-Content $path -Raw) -like "*This file is part of the WingetPathUpdater package*" )
             {
-                if( !$quiet )
+                if( !$quiet -and !$Uninstall )
                 {
                     Write-Host "Looks like it came from us, so we'll just overwrite."
                 }
@@ -68,7 +91,7 @@ try
             {
                 if( $Force )
                 {
-                    if( !$quiet )
+                    if( !$quiet -and !$Uninstall )
                     {
                         Write-Host "Honoring -Force switch to allow overwriting." -Fore Yellow
                     }
@@ -84,43 +107,142 @@ try
             }
         }
 
-        $url = $fileUrls[ $path ]
-
-        if( !$quiet )
+        if( !$Uninstall )
         {
-            Write-Host "fetching: $url" -Fore DarkGray
+            $url = $fileUrls[ $path ]
+
+            if( !$quiet )
+            {
+                Write-Host "fetching: $url" -Fore DarkGray
+            }
+
+            # This will be quick; let's not have a flash of progress bar.
+            $oldPref = $global:ProgressPreference
+            $global:ProgressPreference = 'SilentlyContinue'
+            try
+            {
+                $response = Invoke-WebRequest $url
+            }
+            finally
+            {
+                $global:ProgressPreference = $oldPref
+            }
+
+            if( $response.StatusCode -ne 200 )
+            {
+                throw "Failed to download $(Split-Path -Leaf $path) with error $($response.StatusCode). URL: $url"
+            }
+
+            $fileContents[ $path ] = $response.Content
         }
-
-        $response = Invoke-WebRequest $url
-
-        if( $response.StatusCode -ne 200 )
-        {
-            throw "Failed to download $(Split-Path -Leaf $path) with error $($response.StatusCode). URL: $url"
-        }
-
-        $fileContents[ $path ] = $response.Content
     }
 
     #
-    # We have all the files, let's go!
+    # Ready to go!
     #
 
-    $commonOptions = @{
-        # Using the Ascii encoding (which we can do because none of our files have any
-        # chars > 7bit ascii) sidesteps the problem of the PS 5.1 UTF8 option emitting a
-        # BOM.
-        Encoding = 'Ascii'
-        NoNewline = $true
-        ErrorAction = 'Stop'
-    }
+    $keyPath ='HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Uninstall\WingetPathUpdater'
 
-    foreach( $path in $fileContents.Keys )
+    if( $Uninstall )
     {
+        foreach( $path in $fileUrls.Keys )
+        {
+            if( !$quiet )
+            {
+                Write-Host "Removing $path" -Fore DarkCyan
+            }
+
+            # The file might not actually be there, like for the test file if the original
+            # install didn't install the tests (which would be the common case).
+            Remove-Item $path -Force -EA Ignore
+
+            # The important thing is that the file is not there now:
+            if( (Test-Path $path) )
+            {
+                Write-Warning "Failed to remove: $path"
+            }
+        }
+
         if( !$quiet )
         {
-            Write-Host "Creating: " -NoNewline ; Write-Host $path -Fore Cyan
+            Write-Host "Removing ARP entry..." -Fore DarkCyan
         }
-        Set-Content -Path $path -Value $fileContents[ $path ] @commonOptions
+
+        $null = reg.exe delete $keyPath /f
+        if( $global:LastExitCode )
+        {
+            # (if it failed, it already wrote a message to stderr, which we did not
+            # capture in the assignment to $null)
+            throw "Failed: reg.exe delete $keyPath /f"
+        }
+    }
+    else
+    {
+        # Install!
+
+        $commonOptions = @{
+            # Using the Ascii encoding (which we can do because none of our files have any
+            # chars > 7bit ascii) sidesteps the problem of the PS 5.1 UTF8 option emitting a
+            # BOM.
+            Encoding = 'Ascii'
+            NoNewline = $true
+            ErrorAction = 'Stop'
+        }
+
+        foreach( $path in $fileContents.Keys )
+        {
+            if( !$quiet )
+            {
+                Write-Host "Creating: " -NoNewline ; Write-Host $path -Fore Cyan
+            }
+            Set-Content -Path $path -Value $fileContents[ $path ] @commonOptions
+        }
+
+        if( !$quiet )
+        {
+            Write-Host "Creating ARP entry..." -Fore Cyan
+        }
+
+        #
+        # Since the winget manifest describes how to install an app, you might think that
+        # the winget manifest also describes how to *uninstall* the app. But that's not
+        # how it works: once an app is installed, winget depends on "Windows" to know what
+        # is installed, and how to uninstall it. And so the way we do that is to create an
+        # "Add/Remove Programs" entry (which is just a registry key with a bunch of values
+        # describing the app, including the uninstall command line).
+        #
+
+        $installDate = (Get-Date).ToString('yyyyMMdd')
+        $sizeInKb = [int] (($fileContents.Values | Select-Object -ExpandProperty Length | Measure-Object -Sum).Sum / 1kb)
+
+        $null = reg.exe add $keyPath /f
+        if( $global:LastExitCode )
+        {
+            # (if it failed, it already wrote a message to stderr, which we did not
+            # capture in the assignment to $null)
+            throw "Failed: reg.exe add $keyPath /f"
+        }
+
+        # If the first reg operation succeeded, the others should, too, so we'll
+        # have abbreviated checks.
+        function checkRegResult { if( $global:LastExitCode ) { throw "reg operation failed" } }
+
+        # Perhaps the most important value:
+        $null = reg.exe add $keyPath /f /v UninstallString /d "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command . { try { & \`"$env:SystemRoot\System32\WingetPathUpdaterInstall.ps1\`" -Uninstall -Interactive } catch { Write-Error `$_ ; Start-Sleep -Seconds 10 ; exit -2 } ; exit `$LastExitCode }" ; checkRegResult
+
+        $null = reg.exe add $keyPath /f /v QuietUninstallString /d "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command . { try { & \`"$env:SystemRoot\System32\WingetPathUpdaterInstall.ps1\`" -Uninstall -Silent } catch { Write-Error `$_ ; Start-Sleep -Seconds 10 ; exit -2 } ; exit `$LastExitCode }" ; checkRegResult
+
+        $null = reg.exe add $keyPath /f /v InstallLocation /d "$env:SystemRoot\System32" ; checkRegResult
+        $null = reg.exe add $keyPath /f /v InstallDate /d $installDate ; checkRegResult
+        $null = reg.exe add $keyPath /f /v URLInfoAbout /d "https://github.com/jazzdelightsme/WingetPathUpdater" ; checkRegResult
+        $null = reg.exe add $keyPath /f /v DisplayName /d WingetPathUpdater ; checkRegResult
+        $null = reg.exe add $keyPath /f /v Publisher /d jazzdelightsme ; checkRegResult
+        $null = reg.exe add $keyPath /f /v NoModify /t REG_DWORD /d 1 ; checkRegResult
+        $null = reg.exe add $keyPath /f /v NoRepair /t REG_DWORD /d 1 ; checkRegResult
+        $null = reg.exe add $keyPath /f /v VersionMajor /t REG_DWORD /d 1 ; checkRegResult
+        $null = reg.exe add $keyPath /f /v VersionMinor /t REG_DWORD /d 0 ; checkRegResult
+        $null = reg.exe add $keyPath /f /v DisplayVersion /d 1.0 ; checkRegResult
+        $null = reg.exe add $keyPath /f /v EstimatedSize /t REG_DWORD /d $sizeInKb ; checkRegResult
     }
 
     if( !$quiet )
